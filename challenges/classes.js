bd11addeb03a77562df463584b53b38e93021c1f
CScori <amilanotte1366@gmail.com>
2019-06-15 13:32:25 -0500

    shaky answers for functions

--- challenges/classes.js
+++ challenges/classes.js
@@ -6,11 +6,11 @@
 //     this.height = props.height;
 //   }
 
-class CuboidMaker {
+class Cuboid {
     constructor(props) {
-        this.length = props.length
-        this.width = props.width
-        this.height = props.height
+        this.length = props.length;
+        this.width = props.length;
+        this.height = props.height;
     }
     volume() {
         return `${this.length} * ${this.width} * ${this.height}`;
@@ -21,10 +21,14 @@ class CuboidMaker {
 }
 
 
-
+const window = new Cuboid({
+    length = 4,
+    width = 5,
+    height = 5,
+  })
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,26 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
- 
+ let consume = function (apples, oranges, callback) {
+   callback(apples + oranges);
+ }
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(apples, oranges) {
+  return apples + oranges;
+}
 
+function multiply(apples, oranges) {
+  return apples * oranges;
+}
+
+function greeting(name) {
+  console.log(`Hello ${this.firstName} ${this.lastName}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
