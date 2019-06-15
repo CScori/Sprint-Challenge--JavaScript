a58d575e390d19e542016c9a9b5db416900cf3a8
CScori <amilanotte1366@gmail.com>
2019-06-15 12:45:27 -0500

    semi-answers classes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,28 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+// function CuboidMaker(props) {
+//     this.length = props.length;
+//     this.width = props.width;
+//     this.height = props.height;
+//   }
+
+class CuboidMaker {
+    constructor(props) {
+        this.length = props.length
+        this.width = props.width
+        this.height = props.height
+    }
+    volume() {
+        return `${this.length} * ${this.width} * ${this.height}`;
+    }
+    surfaceArea() {
+        return `2 * (${this.length} * ${this.width} + ${this.length} * ${this.width} * ${this.height} + ${this.width} * ${this.height}`;
+    }
+}
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
